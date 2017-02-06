216651f78417a2cd798dea7b03bea0db8c1a02c6
MrKrabwell <yosuke.kanii@gmail.com>
2017-02-06 17:29:12 -0500

    Program will calculate the batting averages and slugging percentage.  TODO: Need to validate user inputs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -78,44 +78,132 @@ public class Main {
 
 
     /**
+     * This is the method to get the batting average for a player
+     * @param batterStat
+     * @return
+     */
+    public static double getBattingAverage(int[] batterStat) {
+        // Variable declaration
+        int numHits = 0;          // number of hits, i.e. non-zero value
+
+        // Count the numer of hits per element
+        for (int i = 0; i < batterStat.length; i++) {
+            if (batterStat[i] != 0) {
+                ++numHits;
+            }
+        }
+
+        // return the batting average = non-zero/numatbats
+        return (double)numHits / (double)batterStat.length;
+    }
+
+
+    /**
+     * This is the method to get the slugging percentage for a player
+     * @param batterStat
+     * @return
+     */
+    public static double getSluggingPercentage(int[] batterStat) {
+        int sum = 0;        // sum of all the at bat values
+
+        for (int element : batterStat) {
+            sum = sum + element;
+        }
+
+        // return the slugging percentage sum/numatbats
+        return (double)sum / (double)batterStat.length;
+    }
+
+    /*************************************************************************
+     * This method asks the user if they want to continue the program or not *
+     *************************************************************************/
+    public static boolean askUserToContinue() {
+        // Variable declarations
+        String userInput = "";                  // User input string
+        Scanner scan = new Scanner(System.in);  // Scanner object for user input
+
+        // Prompt user to enter yes or no
+        System.out.print("Do you want to continue? (y/n): ");
+        userInput = scan.nextLine();
+        System.out.println("");
+
+        // Validate whether user input is ok, and continue asking until right
+        while (!userInput.equalsIgnoreCase("y") &&
+                !userInput.equalsIgnoreCase("yes") &&
+                !userInput.equalsIgnoreCase("n") &&
+                !userInput.equalsIgnoreCase("no")) {
+            System.out.println("That is not a valid input.  Please try again. ");
+            System.out.print("Do you want to continue? (y/n): ");
+            userInput = scan.nextLine();
+            System.out.println("");
+        }
+
+        // Return true if user says yes, and false if user says no
+        if (userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
      * This is the main method
      * @param args
      */
     public static void main(String[] args) {
 	    // Variable declarations
-        int numBatters = 0;       // holds how many batters there are to store data
-        int[][] batterStats;      // Array to hold the array of batter stats
+        int numBatters = 0;               // holds how many batters there are to store data
+        int[][] batterStats;              // Array to hold the array of batter stats
+        boolean continueProgram = true;   // boolean to keep the program running
 
         // Welcome message
         System.out.println("Welcome to the Batting Average Calculator!\n");
 
-        // create the batter stats matrix, with number of players of rows
-        batterStats = new int[getNumBatters()][];
+        // Continue the program until user decides to quit
+        while (continueProgram) {
 
-        // Fill out the player stats for each player
-        for (int i = 0; i < batterStats.length; i++) {
-            // Create the number of columns per player index i
-            batterStats[i] = new int[getNumOfAtBats(i)];
+            // create the batter stats matrix, with number of players of rows
+            batterStats = new int[getNumBatters()][];
+            System.out.println();
 
-            // Heading for results
-            System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
-            // Fill out the columns i.e. at bats
-            for (int j = 0; j < batterStats[i].length; j++) {
-                batterStats[i][j] = getAtBatResult(j);
-            }
-        }
+            // Fill out the player stats for each player
+            for (int i = 0; i < batterStats.length; i++) {
+                // Create the number of columns per player index i
+                batterStats[i] = new int[getNumOfAtBats(i)];
+                System.out.println();
 
-        // Now you have the jagged array batterStats that holds the stats for each player
-        // Testing delete later
-        System.out.println("This is the jagged matrix");
-        for (int i = 0; i < batterStats.length; i++) {
-            for (int j = 0; j < batterStats[i].length; j++) {
-                System.out.printf("%-5d", batterStats[i][j]);
+                // Heading for results
+                System.out.println("0=out, 1=single, 2=double, 3=triple, 4=home run");
+
+                // Fill out the columns i.e. at bats results
+                for (int j = 0; j < batterStats[i].length; j++) {
+                    batterStats[i][j] = getAtBatResult(j);
+                }
+                System.out.println();
             }
             System.out.println();
-        }
 
-        
+            // Now you have the jagged array batterStats that holds the stats for each player
+            /* Testing delete later
+            System.out.println("This is the jagged matrix");
+            for (int i = 0; i < batterStats.length; i++) {
+                for (int j = 0; j < batterStats[i].length; j++) {
+                    System.out.printf("%-5d", batterStats[i][j]);
+                }
+                System.out.println();
+            }*/
+
+            // Print results for each batter
+            for (int i = 0; i < batterStats.length; i++) {
+                System.out.printf("Batter %5d average: %-10.3f slugging percentage: %-10.3f", i+1,
+                        getBattingAverage(batterStats[i]), getSluggingPercentage(batterStats[i]));
+                System.out.println();
+            }
+            System.out.println();
+
+            // Ask the user if they want to continue
+            continueProgram = askUserToContinue();
+        }
 
     }
 }
